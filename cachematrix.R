31d1c7b405b9362f315cb1515e8ee938e8d23d82
raosindhu <sindhureddy.195@gmail.com>
2016-02-01 14:21:15 -0800

    Week 3 coursera: Matrix cache

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,21 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverse <- NULL
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+  cachedinv <- x$getInv()
+  if(!is.null(cachedinv)) {
+    message("getting cached inverse")
+    return(cachedinv)
+  }
+  matx <- x$get()
+  calcinv <- solve(matx, ...)
+  x$setInv(calcinv)
+  calcinv
 }
